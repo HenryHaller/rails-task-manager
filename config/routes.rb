26799907433729ef005752ffa2e98735d31b9f4c
Henry Haller <henrybhaller@gmail.com>
2018-08-07 15:35:18 +0900

    basic to-do list working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    "tasks",          to: "tasks#index", as: :index
+
+  get    "tasks/new",      to: "tasks#new", as: :new
+    #VIEW ONE#
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "tasks/:id",      to: "tasks#show", as: :show
+    #CREATE#
+  post   "tasks",          to: "tasks#create"
+
+
+   #UPDATE#
+   #put new information in a form#
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+    #send form's information into the server so the server can update the model with the new information#
+  patch  "tasks/:id",      to: "tasks#update", as: :update
+
+    #DELETE#
+  delete "tasks/:id",      to: "tasks#destroy", as: :delete
 end
+
